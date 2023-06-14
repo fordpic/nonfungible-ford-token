8f0e53ba1ea572d504f39646bc0e0c821fd3a363
Ford Pickert <fordpickert@gmail.com>
2023-06-14 16:46:58 -0500

    add foundry devops package, spin up makefile

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {NFordT} from "../src/NFordT.sol";
 
 contract MintNFordT is Script {
-    function run() external {}
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "NFordT",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        NFordT(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
 }
