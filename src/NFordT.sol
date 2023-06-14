2013ffec7ea2f493b164d6e1c3f9abf3ec2bc9d3
Ford Pickert <fordpickert@gmail.com>
2023-06-13 19:37:36 -0500

    overriding uri, img in

--- src/NFordT.sol
+++ src/NFordT.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFordT {}
+contract NFordT is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("NFordT", "N4DT") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {}
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
+}
