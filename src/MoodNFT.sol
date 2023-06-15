98e20dae8f017ae0c3eb9d20ca1abb30ac69ca60
Ford Pickert <fordpickert@gmail.com>
2023-06-15 16:06:42 -0500

    flipMood func

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -5,6 +5,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721 {
+    error MoodNFT__CantFlipMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
@@ -29,6 +31,17 @@ contract MoodNFT is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY; // default to happy
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // only owner can call
+        if (!_isApprovedOrOwner(msg.sender, tokenId))
+            revert MoodNFT__CantFlipMoodIfNotOwner();
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
